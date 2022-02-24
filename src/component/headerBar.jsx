7a57646cbb899c44a7910fdf9a084eeacbe8d631
Mohd Rejoan <wearecrx@gmail.com>
2022-02-24 22:32:58 +0600

    header section complete

--- src/component/headerBar.jsx
+++ src/component/headerBar.jsx
@@ -10,7 +10,7 @@ const HeaderBar = (props) => {
 
     return (
         <section>
-            <div className="container mx-auto flex flex-wrap justify-between">
+            <div className="container mx-auto flex flex-wrap justify-between sm:items-center">
                 {/* logo  */}
                 <a href="" id="logo" className="text-center text-black order-1">
                     <h1 className="font-black uppercase font-sans text-2xl">
@@ -20,31 +20,38 @@ const HeaderBar = (props) => {
                     </h1>
                 </a>
                 {/* page links  */}
-                
-                    <div className={`${navShow? 'flex': 'hidden'} flex-col justify-between w-full order-3 bg-stone-500 mt-4 rounded-lg py-4 sm:flex`}>
-                        <a
-                            href=""
-                            className="text-white text-sm font-medium px-12 flex items-center rounded-md mb-2 transition-all hover:bg-[#AEAFFF]"
-                        >
-                            About
-                        </a>
-                        <a
-                            href=""
-                            className="text-white text-sm font-medium px-12 flex items-center rounded-md mb-2 transition-all hover:bg-[#AEAFFF]"
-                        >
-                            FAQs
-                        </a>
-                        <a
-                            href=""
-                            className="text-white text-sm font-medium px-12 flex items-center rounded-md transition-all hover:bg-[#AEAFFF]"
-                        >
-                            Contact Us
-                        </a>
-                    </div>
-                
+
+                <div
+                    className={`${
+                        navShow ? "flex" : "hidden"
+                    } flex-col justify-between w-full order-3 bg-stone-500 mt-4 rounded-lg py-4
+                    sm:flex sm:flex-row sm:w-9/12 sm:bg-transparent sm:mt-0 sm:justify-end`}
+                >
+                    <a
+                        href=""
+                        className="text-white text-sm font-medium px-8 flex items-center rounded-md xl:px-10 mxl:px-12 mb-2 sm:mb-0 transition-all hover:bg-[#AEAFFF]"
+                    >
+                        About
+                    </a>
+                    <a
+                        href=""
+                        className="text-white text-sm font-medium px-8 flex items-center rounded-md xl:px-10 mxl:px-12 mb-2 sm:mb-0 transition-all hover:bg-[#AEAFFF]"
+                    >
+                        FAQs
+                    </a>
+                    <a
+                        href=""
+                        className="text-white text-sm font-medium px-8 flex items-center rounded-md xl:px-10 mxl:px-12 transition-all hover:bg-[#AEAFFF]"
+                    >
+                        Contact Us
+                    </a>
+                </div>
 
                 {/* resp navbar  */}
-                <button onClick={navClickHandler} className=" w-[30px] order-2 sm:hidden">
+                <button
+                    onClick={navClickHandler}
+                    className=" w-[30px] order-2 sm:hidden"
+                >
                     <span className="a w-full h-1 block rounded-sm bg-white mb-[5px]"></span>
                     <span className="a w-full h-1 block rounded-sm bg-white mb-[5px]"></span>
                     <span className="a w-full h-1 block rounded-sm bg-white"></span>
