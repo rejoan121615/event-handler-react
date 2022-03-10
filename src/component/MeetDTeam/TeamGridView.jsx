1d3193dbed6d39563757214a77a681e2374e99a8
Mohd Rejoan <wearecrx@gmail.com>
2022-03-10 18:42:03 +0600

    random color added, slide added

--- src/component/MeetDTeam/TeamGridView.jsx
+++ src/component/MeetDTeam/TeamGridView.jsx
@@ -8,11 +8,15 @@ const TeamGridView = () => {
     // console.log(team);
 
     return (
-        <div className="grid mt-10 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-6 xl:gap-y-10 2xl:grid-cols-4 2xl:gap-y-5 2xl:gap-x-4 mxl:gap-y-12 mxl:gap-x-16">
-            {team.team.map((team, index) => {
-                return <TeamCard {...team} key={index + "i"} />;
-            })}
-        </div>
+        <section>
+            <div className="container">
+                <div className="grid mt-10 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-6 xl:gap-y-10 2xl:grid-cols-4 2xl:gap-y-5 2xl:gap-x-4 mxl:gap-y-12 mxl:gap-x-16">
+                    {team.team.map((team, index) => {
+                        return <TeamCard {...team} key={index + "i"} />;
+                    })}
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/ui/MeetDTean/TeamCard.jsx
+++ src/ui/MeetDTean/TeamCard.jsx
@@ -1,33 +1,9 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ProfileGenerator from "./ProfileGenerator";
 
 const TeamCard = (props) => {
-    const [profile, setProfile] = useState(props.profiles);
-
-    const genPro = (type) => {
-        switch (type) {
-            case "github":
-                return <FontAwesomeIcon icon={["fab", "github"]} />;
-            case "js":
-                return <FontAwesomeIcon icon={["fab", "js"]} />;
-            case "dribble":
-                return <FontAwesomeIcon icon={["fab", "dribbble"]} />;
-            default:
-                return <FontAwesomeIcon icon={"gem"} />;
-        }
-    };
-
-    const proList = profile.map((item, index) => {
-        return (
-            <a
-                href={item.link}
-                className=" text-white w-8 h-8 text-[18px] bg-[#646e73] flex justify-center items-center rounded-full"
-                key={index}
-            >
-                {genPro(item.type)}
-            </a>
-        );
-    });
+    
 
     return (
         <div className=" flex gap-x-6 lg:flex-col xl:flex-row">
@@ -45,7 +21,7 @@ const TeamCard = (props) => {
                 </h3>
                 <p className=" mt-4 text-white">{props.about}</p>
                 {/* bottom links  */}
-                <div className=" mt-4 flex gap-x-3">{proList}</div>
+                <ProfileGenerator {...props} />
             </div>
         </div>
     );
